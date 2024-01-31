6f42e0bea7bc495c0937bc10c4596d24b05104b8
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-31 11:03:37 +0300

    fix 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -29,7 +29,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 				node->parent = parent;
 				node->left = temp;
 				temp->parent = node;
-				node->parent->left = node;
+				parent->left = node;
 			}
 		}
 	}
