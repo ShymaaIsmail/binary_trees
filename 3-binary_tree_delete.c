a20d5b6215d4f598b76c88af5ccb31247ab188fc
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-31 12:01:54 +0300

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_delete - binary_tree_delete
+* @tree: root node
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			free(tree);
+		}
+	}
+}
