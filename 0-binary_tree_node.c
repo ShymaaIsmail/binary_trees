88427defe82c71e45bff5cfef1e06146b2530fdf
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-31 09:34:00 +0300

    task 0 and project skeleton

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - binary_tree_node
+* @parent: parent node
+* @value: value
+* Return: node pointer
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node != NULL)
+	{
+		node->n = value;
+		if (parent != NULL)
+		{
+			node->parent = parent;
+		}
+	}
+	return (node);
+}
