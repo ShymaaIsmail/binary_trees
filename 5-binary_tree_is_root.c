3bd52cc888322e206fca7360e6857bd0abb4017e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-31 12:15:04 +0300

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_is_root - binary_tree_is_root
+* @node:  node
+* Return: int 1 or 0
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	int is_root = 0;
+
+	if (node != NULL && (node->left  !=  NULL || node->right != NULL))
+	{
+		is_root = 1;
+	}
+	return (is_root);
+}
