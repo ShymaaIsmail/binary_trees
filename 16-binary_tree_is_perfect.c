b4a049bf6c2c82326b946c893d66b467ee5545ed
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 11:40:02 +0300

    fix perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,20 +6,27 @@
 * @tree: tree node
 * Return: int for height
 */
-int get_tree_height(const binary_tree_t *tree)
+size_t get_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height = 0;
+	size_t height = 0, left_height = 0, right_height = 0, max_height = 0;
 
-	if (tree == NULL)
+	if (tree != NULL)
 	{
-		return (0);
+		left_height = get_tree_height(tree->left);
+		right_height = get_tree_height(tree->right);
+		max_height = (left_height > right_height) ? left_height : right_height;
+		if (tree->left != NULL || tree->right != NULL)
+		{
+			height = 1 + max_height;
+		}
+		else
+		{
+			height = max_height;
+		}
 	}
-
-	left_height  = get_tree_height(tree->left);
-	right_height = get_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (height);
 }
+
 
 /**
 * binary_tree_is_perfect - binary_tree_is_perfect
