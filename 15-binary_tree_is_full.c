2a7f3cf4c5ec56f2f1d1eac6bd6d6c84b2cc78a6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 10:37:44 +0300

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+
+
+/**
+* binary_tree_is_full - binary_tree_is_full
+* @tree: tree node
+* Return: 1 or 0 is full flag allnodes are full with 2 childs
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int is_full = 0;
+
+	if (tree != NULL && tree->left != NULL && tree->right != NULL)
+	{
+		is_full = 1;
+	}
+	return (is_full);
+}
