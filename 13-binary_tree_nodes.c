41cee897c08883f9542815e1ea4f990d2103014b
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 10:06:01 +0300

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,7 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
-		if (tree->left == NULL || tree->right == NULL)
+		if (tree->left != NULL || tree->right != NULL)
 		{
 			nodes += 1;
 		}
