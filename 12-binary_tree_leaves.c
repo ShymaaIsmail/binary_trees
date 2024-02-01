3398653127891272187fe679e13940ab7e4708d0
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 10:00:08 +0300

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+
+
+/**
+* binary_tree_leaves - binary_tree_leaves
+* @tree: tree root node
+* Return: leaves of tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree != NULL)
+	{
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			leaves += 1;
+		}
+	}
+	return (leaves);
+}
