8a800269088c15d1e13c28b8ee1436c8fbecd909
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 11:55:28 +0300

    fix 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node != NULL && node->parent != NULL)
 	{
 		sibling = node->parent->left == node ?
-					node->parent->right : node->parent->left
+					node->parent->right : node->parent->left;
 	}
 	return (sibling);
 }
