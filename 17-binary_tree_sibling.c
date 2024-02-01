6d003cf39594bee38ffa5c0faf05e0e128ecadea
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-01 11:55:03 +0300

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_sibling - binary_tree_sibling
+* @node: tree node
+* Return: sibling node
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *sibling = NULL;
+
+	if (node != NULL && node->parent != NULL)
+	{
+		sibling = node->parent->left == node ?
+					node->parent->right : node->parent->left
+	}
+	return (sibling);
+}
